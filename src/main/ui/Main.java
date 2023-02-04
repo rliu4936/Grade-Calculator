7c1d601a830a71748cfd204d5407789728d01d02
Ronald Liu <rliu4936@gmail.com>
2023-02-03 18:29:20 -0800

    added description to README.md

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,9 +1,30 @@
 package ui;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static final List<String> listOfCommands = Arrays.asList("Add Course <name>", "Select Course <name>", "Quit");
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            System.out.println("Commands:");
+            for (int i = 0; i < listOfCommands.size(); i++) {
+                System.out.println(i + 1 + ". " + listOfCommands.get(i));
+            }
+            System.out.println("Please input your command (number or full string command): ");
+            String command = sc.nextLine();
+            switch (command) {
+                case "quit":
+                    run = false;
+                    break;
+            }
+
+        }
     }
 }
