0bc80bf5f9cf53d72058e0bd8953d37e7f27b2c8
Ronald Liu <rliu4936@gmail.com>
2023-02-25 20:19:13 -0800

    updated testFindLetterGrade

--- src/test/model/MyModelTest.java
+++ src/test/model/MyModelTest.java
@@ -80,17 +80,48 @@ class MyModelTest {
 
     @Test
     void testFindLetterGrade() {
-        Course c1 = new Course("MATH 100");
+        Course c1 = new Course("MATH 101");
         c1.addGradingGroup("Final", 100, 90);
         assertEquals("A+", c1.findLetterGrade());
 
-        Course c2 = new Course("MATH 101");
-        c2.addGradingGroup("Final", 100, 50);
-        assertEquals("D", c2.findLetterGrade());
+        Course c2 = new Course("MATH 102");
+        c2.addGradingGroup("Final", 100, 85);
+        assertEquals("A", c2.findLetterGrade());
 
-        Course c3 = new Course("MATH 102");
-        c3.addGradingGroup("Final", 100, 45);
-        assertEquals("F", c3.findLetterGrade());
+        Course c3 = new Course("MATH 103");
+        c3.addGradingGroup("Final", 100, 80);
+        assertEquals("A-", c3.findLetterGrade());
 
+        Course c4 = new Course("MATH 104");
+        c4.addGradingGroup("Final", 100, 76);
+        assertEquals("B+", c4.findLetterGrade());
+
+        Course c5 = new Course("MATH 105");
+        c5.addGradingGroup("Final", 100, 72);
+        assertEquals("B", c5.findLetterGrade());
+
+        Course c6 = new Course("MATH 106");
+        c6.addGradingGroup("Final", 100, 68);
+        assertEquals("B-", c6.findLetterGrade());
+
+        Course c7 = new Course("MATH 107");
+        c7.addGradingGroup("Final", 100, 64);
+        assertEquals("C+", c7.findLetterGrade());
+
+        Course c8 = new Course("MATH 108");
+        c8.addGradingGroup("Final", 100, 60);
+        assertEquals("C", c8.findLetterGrade());
+
+        Course c9 = new Course("MATH 109");
+        c9.addGradingGroup("Final", 100, 55);
+        assertEquals("C-", c9.findLetterGrade());
+
+        Course c10 = new Course("MATH 110");
+        c10.addGradingGroup("Final", 100, 50);
+        assertEquals("D", c10.findLetterGrade());
+
+        Course c11 = new Course("MATH 111");
+        c11.addGradingGroup("Final", 100, 45);
+        assertEquals("F", c11.findLetterGrade());
     }
 }
